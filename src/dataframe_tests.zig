758f47a085f72cf875f661f74cc497b1c5976f00
passchaos <greedwolf.dss@gmail.com>
2026-07-29 06:28:54 +0800

    Split dataframe sort profile tests out

--- src/dataframe_tests.zig
+++ src/dataframe_tests.zig
@@ -1,5 +1,6 @@
 test {
     _ = @import("dataframe_eager_tests.zig");
+    _ = @import("dataframe_sort_profile_tests.zig");
     _ = @import("dataframe_profile_tests.zig");
     _ = @import("dataframe_lazy_collect_tests.zig");
     _ = @import("dataframe_join_tests.zig");
