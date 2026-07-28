ead221c875d35ae7d41d57093d9599e8c18c87fa
passchaos <greedwolf.dss@gmail.com>
2026-07-29 06:27:08 +0800

    Split dataframe lazy collect tests out

--- src/dataframe_tests.zig
+++ src/dataframe_tests.zig
@@ -1,6 +1,7 @@
 test {
     _ = @import("dataframe_eager_tests.zig");
     _ = @import("dataframe_profile_tests.zig");
+    _ = @import("dataframe_lazy_collect_tests.zig");
     _ = @import("dataframe_join_tests.zig");
     _ = @import("dataframe_parquet_tests.zig");
 }
